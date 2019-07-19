05bafc421cea0584cffe13465938b40f44b9f364
asnimansari <asnim96@gmail.com>
2019-07-19 22:09:08 +0530

    Testing Travis CI

--- complex_test.go
+++ complex_test.go
@@ -9,3 +9,18 @@ func TestComplex128_Absolute(t *testing.T) {
 		t.Error("Expected 5 , got ",c)
 	}
 }
+
+
+
+
+func TestComplex128_GetImaginary(t *testing.T) {
+	imaginary:=Complex128{3,4}.GetImaginary()
+	if imaginary!=10{
+		t.Error("Expected 4 , got ",imaginary)
+	}
+}
+
+
+
+
+
